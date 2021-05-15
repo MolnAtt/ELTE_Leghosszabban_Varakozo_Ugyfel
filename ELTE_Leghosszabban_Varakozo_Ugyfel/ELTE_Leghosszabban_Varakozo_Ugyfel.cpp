5c046279a4c60144179dbecc7f8ec07653917510
MolnAtt <molnar.attila@szlgbp.hu>
2021-05-15 20:33:38 +0200

    unresolved external symbol

--- ELTE_Leghosszabban_Varakozo_Ugyfel/ELTE_Leghosszabban_Varakozo_Ugyfel.cpp
+++ ELTE_Leghosszabban_Varakozo_Ugyfel/ELTE_Leghosszabban_Varakozo_Ugyfel.cpp
@@ -1,59 +1,106 @@
 
 #include <iostream>
+#include <vector>
 #include <queue>
 
 using namespace std;
 
 struct Ugyfel 
 {
+    static vector<Ugyfel> lista;
+    static Ugyfel leghosszabban_varakozo;
+
+    int sorszam;
     int erk;
     int ugyi;
+    int varakozasi_ido;
 
-    Ugyfel(int e, int u)
+    Ugyfel(int s, int e, int u)
     {
+        sorszam = s;
         erk = e;
         ugyi = u;
     }
+
+    static void rendez() { rendez(0, lista.size() - 1); }
+    static void rendez(int e, int v)
+    {
+        if (e < v)
+        {
+            int i = e;
+            int j = v;
+            while (i != j)
+            {
+                if ((i < j) != (lista[i].erk < lista[j].erk))
+                {
+                    swap(lista[i], lista[j]);
+                    swap(i, j);
+                }
+                j += i < j ? -1 : 1;
+            }
+            rendez(e, j - 1);
+            rendez(j + 1, v);
+        }
+    }
+
+    static void Kiir()
+    {
+        cerr << "------------------------------\n";
+        for (Ugyfel& ugyfel : lista)
+        {
+            cerr << ugyfel.sorszam << " " << ugyfel.erk << " " << ugyfel.ugyi;
+        }
+        cerr << "------------------------------\n";
+    }
 };
 
-struct Input
+void Beolvas()
 {
-    static const int n_MAX = 10000;
-    int n;
-    int e[n_MAX];
-    int u[n_MAX];
-};
-
-
-Input Beolvas()
-{
-    Input be;
-
+    int ei, ui, db;
     cerr << "Adja meg, hogy hany ugyfel adatait fogja megadni: \n";
-    cin >> be.n;
-    for (int i = 0; i < be.n; i++)
+    cin >> db;
+
+    for (int i = 0; i < db; i++)
     {
         cerr << "A(z) " << i + 1 << ". ugyfel erkezesi ideje es ugyintezesi ideje: \n";
-        cin >> be.e[i] >> be.u[i];
+        cin >> ei >> ui;
+        Ugyfel::lista.push_back(Ugyfel(i, ei, ui));
     }
-    return be;
 }
 
-int Feldolgoz(Input be)
+Ugyfel Feldolgoz()
 {
+    priority_queue<Ugyfel> sor;
+
+    cerr << "Ugyfelek eredetileg:\n";
+    Ugyfel::Kiir();
+
+    // 1. érkezési sorrendbe rakjuk az ügyfeleket:
+
+    Ugyfel::rendez();
+
+    cerr << "Ugyfelek erkezesi sorrendben:\n";
+    Ugyfel::Kiir();
 
 
-    return -1;
+    Ugyfel leghosszabban_varakozo = Ugyfel::lista[0];
+    
+    // Szimuláció
+
+    return leghosszabban_varakozo;
 }
 
-void Kiir(int ki)
+void Kiir(Ugyfel leghosszabban_varakozo)
 {
     cerr << "--------------------------------------------------\nA leghosszabban varakozo ugyfel sorszama (tobb eseten a legelso): ";
-    cout << ki;
+    cout << leghosszabban_varakozo.sorszam;
 }
+
+
 
 int main()
 {
-    Kiir(Feldolgoz(Beolvas()));
+    Beolvas();
+    Kiir(Feldolgoz());
 }
 
