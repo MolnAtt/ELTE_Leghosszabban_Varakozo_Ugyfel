16c8cdb54ed370981cd9499476cbe6e8eb8170af
MolnAtt <molnar.attila@szlgbp.hu>
2021-05-16 08:00:01 +0200

    elbúcsúztunk a statictól

--- ELTE_Leghosszabban_Varakozo_Ugyfel/ELTE_Leghosszabban_Varakozo_Ugyfel.cpp
+++ ELTE_Leghosszabban_Varakozo_Ugyfel/ELTE_Leghosszabban_Varakozo_Ugyfel.cpp
@@ -7,9 +7,6 @@ using namespace std;
 
 struct Ugyfel 
 {
-    static vector<Ugyfel> lista;
-    static Ugyfel leghosszabban_varakozo;
-
     int sorszam;
     int erk;
     int ugyi;
@@ -20,43 +17,49 @@ struct Ugyfel
         sorszam = s;
         erk = e;
         ugyi = u;
+        varakozasi_ido = -1;
     }
 
-    static void rendez() { rendez(0, lista.size() - 1); }
-    static void rendez(int e, int v)
-    {
-        if (e < v)
-        {
-            int i = e;
-            int j = v;
-            while (i != j)
-            {
-                if ((i < j) != (lista[i].erk < lista[j].erk))
-                {
-                    swap(lista[i], lista[j]);
-                    swap(i, j);
-                }
-                j += i < j ? -1 : 1;
-            }
-            rendez(e, j - 1);
-            rendez(j + 1, v);
-        }
-    }
+    
 
-    static void Kiir()
-    {
-        cerr << "------------------------------\n";
-        for (Ugyfel& ugyfel : lista)
-        {
-            cerr << ugyfel.sorszam << " " << ugyfel.erk << " " << ugyfel.ugyi;
-        }
-        cerr << "------------------------------\n";
-    }
 };
 
-void Beolvas()
+void rendez(vector<Ugyfel>& L, int e, int v)
+{
+    if (e < v)
+    {
+        int i = e;
+        int j = v;
+        while (i != j)
+        {
+            if ((i < j) != (L[i].erk < L[j].erk))
+            {
+                swap(L[i], L[j]);
+                swap(i, j);
+            }
+            j += i < j ? -1 : 1;
+        }
+        rendez(L, e, j - 1);
+        rendez(L, j + 1, v);
+    }
+}
+
+void rendez(vector<Ugyfel>& L) { rendez(L, 0, L.size() - 1); }
+
+static void Kiir(vector<Ugyfel> ugyfelek)
+{
+    cerr << "------------------------------\n";
+    for (Ugyfel& ugyfel : ugyfelek)
+    {
+        cerr << ugyfel.sorszam << " " << ugyfel.erk << " " << ugyfel.ugyi << endl;
+    }
+    cerr << "------------------------------\n";
+}
+
+vector<Ugyfel> Beolvas()
 {
     int ei, ui, db;
+    vector<Ugyfel> ugyfelek;
     cerr << "Adja meg, hogy hany ugyfel adatait fogja megadni: \n";
     cin >> db;
 
@@ -64,30 +67,9 @@ void Beolvas()
     {
         cerr << "A(z) " << i + 1 << ". ugyfel erkezesi ideje es ugyintezesi ideje: \n";
         cin >> ei >> ui;
-        Ugyfel::lista.push_back(Ugyfel(i, ei, ui));
+        ugyfelek.push_back(Ugyfel(i, ei, ui));
     }
-}
-
-Ugyfel Feldolgoz()
-{
-    priority_queue<Ugyfel> sor;
-
-    cerr << "Ugyfelek eredetileg:\n";
-    Ugyfel::Kiir();
-
-    // 1. érkezési sorrendbe rakjuk az ügyfeleket:
-
-    Ugyfel::rendez();
-
-    cerr << "Ugyfelek erkezesi sorrendben:\n";
-    Ugyfel::Kiir();
-
-
-    Ugyfel leghosszabban_varakozo = Ugyfel::lista[0];
-    
-    // Szimuláció
-
-    return leghosszabban_varakozo;
+    return ugyfelek;
 }
 
 void Kiir(Ugyfel leghosszabban_varakozo)
@@ -97,10 +79,37 @@ void Kiir(Ugyfel leghosszabban_varakozo)
 }
 
 
+Ugyfel Feldolgoz(vector<Ugyfel> ugyfelek)
+{
+    priority_queue<Ugyfel> sor;
+
+    cerr << "Ugyfelek eredetileg:\n";
+    Kiir(ugyfelek);
+
+    // 1. érkezési sorrendbe rakjuk az ügyfeleket:
+
+    rendez(ugyfelek);
+
+    cerr << "Ugyfelek erkezesi sorrendben:\n";
+    Kiir(ugyfelek);
+
+    for (Ugyfel& ugyfel : ugyfelek)
+    {
+
+    }
+
+    Ugyfel leghosszabban_varakozo = ugyfelek[0];
+    
+    // Szimuláció
+
+    return leghosszabban_varakozo;
+}
+
+
+
 
 int main()
 {
-    Beolvas();
-    Kiir(Feldolgoz());
+    Kiir(Feldolgoz(Beolvas()));
 }
 
